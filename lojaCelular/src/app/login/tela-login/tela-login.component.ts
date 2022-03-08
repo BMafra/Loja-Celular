caa9c1d6650976dffe26aafb216339e340360d01
BMafra <bruna_a_mafra@estudante.sesisenai.org.br>
2022-03-07 21:52:15 -0300

    commit

--- lojaCelular/src/app/login/tela-login/tela-login.component.ts
+++ lojaCelular/src/app/login/tela-login/tela-login.component.ts
@@ -43,7 +43,7 @@ export class TelaLoginComponent implements OnInit {
 
   login() {
     this.usuariosService.buscarClientes()
-      .then((resultado: (Object: String) => []) => {
+      .then((resultado: any) => {
         for (let i = 0; i < resultado.length; i++) {
           if (resultado[i].SENHA === this.password && resultado[i].USER === this.user) {
             this.router.navigate(['carrinho']);
